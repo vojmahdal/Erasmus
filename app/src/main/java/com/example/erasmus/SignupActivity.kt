a630cbdc791a433a7363cbfbfc8e6c3b2683660a
vojtamahdal <vojta@peko-studio.cz>
2024-03-27 23:42:31 +0100

    Adding Firebase database

---
+++ app/src/main/java/com/example/erasmus/SignupActivity.kt
@@ -0,0 +1,59 @@
+package com.example.erasmus
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.erasmus.databinding.ActivitySignupBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import org.w3c.dom.Text
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+        //binding.sign
+    }
+
+    private fun singupUser(username: String, password: String){
+        databaseReference.orderByChild("uername").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(!dataSnapshot.exists()){
+                    val id = databaseReference.push().key
+                    val userData = UserData(id, username, password)
+                    databaseReference.child(id!!).setValue(userData)
+                    Toast.makeText(this@SignupActivity, "Signup successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(this@SignupActivity, "User already exists", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@SignupActivity, "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
