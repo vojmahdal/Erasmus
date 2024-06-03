d9f1fa2f38b494879e43742a500d2f5211032563
vojtamahdal <vojta@peko-studio.cz>
2024-06-03 20:16:42 +0200

    repair dark theme

--- app/src/main/java/com/example/erasmus/SettingsActivity.kt
+++ app/src/main/java/com/example/erasmus/SettingsActivity.kt
@@ -2,9 +2,12 @@ package com.example.erasmus
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,7 +25,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
+        applyTheme()
 
         langPreference = LangPreference(this)
 
@@ -37,40 +40,66 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         val button = findViewById<Button>(R.id.button)
-
+        val radiogrp = findViewById<RadioGroup>(R.id.radioGroup)
+        val radiolight = findViewById<RadioButton>(R.id.btnRadioLight)
+        val radiodark = findViewById<RadioButton>(R.id.btnRadioDark)
+        val radiodevice = findViewById<RadioButton>(R.id.btnRadioDevice)
         button.setOnClickListener {
+            val selectedId = radiogrp.checkedRadioButtonId
+
+            when(selectedId){
+                radiolight.id -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                radiodark.id -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                radiodevice.id -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+            }
+            saveThemePreference(selectedId)
+
+            //for language set
             langPreference.setLoginCount(languageList[spinner.selectedItemPosition])
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-        val switch = findViewById<SwitchCompat>(R.id.switch1)
 
-        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val nightMode = sharedPreferences.getBoolean("night", false)
 
-        if(nightMode){
-            switch.isChecked = true
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
 
-     /*   enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-        switch.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked){
+    }
+
+    private fun applyTheme() {
+
+        val radiogrp = findViewById<RadioGroup>(R.id.radioGroup)
+        val radiolight = findViewById<RadioButton>(R.id.btnRadioLight)
+        val radiodark = findViewById<RadioButton>(R.id.btnRadioDark)
+        val radiodevice = findViewById<RadioButton>(R.id.btnRadioDevice)
+
+
+        val sharedPreferences: SharedPreferences = getPreferences(MODE_PRIVATE)
+        val selectedId = sharedPreferences.getInt("selected_theme", radiodevice.id)
+
+        radiogrp.check(selectedId)
+        when(selectedId){
+            radiolight.id -> {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                editor.putBoolean("night", false)
-                editor.apply()
-            }else{
+            }
+            radiodark.id -> {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                editor.putBoolean("night", true)
-                editor.apply()
+            }
+            radiodevice.id -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
             }
         }
 
+    }
+
+    private fun saveThemePreference(selectedId: Int) {
+        val sharedPreferences: SharedPreferences = getPreferences(MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt("selected_theme", selectedId)
+        editor.apply()
     }
 }
