037963959e2e7f8e23e1822d71b94e57c6601264
vojtamahdal <vojta@peko-studio.cz>
2024-06-03 20:16:42 +0200

    toolbar

--- app/src/main/java/com/example/erasmus/MainActivity.kt
+++ app/src/main/java/com/example/erasmus/MainActivity.kt
@@ -3,20 +3,13 @@ package com.example.erasmus
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.erasmus.databinding.ActivityMainBinding
 import com.example.erasmus.language.LangPreference
-import com.google.firebase.Firebase
-import com.google.firebase.appcheck.appCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.initialize
-
-
-
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,23 +18,23 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var langPreference: LangPreference
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         firebaseAuth = FirebaseAuth.getInstance()
 
-      /*  Firebase.initialize(context = this)
-        Firebase.appCheck.installAppCheckProviderFactory(
-            DebugAppCheckProviderFactory.getInstance(),
-        )*/
-        enableEdgeToEdge()
         setContentView(binding.root)
+
+        // Set up the toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         binding.mainLogout.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
